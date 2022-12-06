cf830d33bc19fcf596d248b07e71431953cb7a81
Admin O <olexandro@gmail.com>
2022-12-06 19:27:38 +0300

    Реализация

---
+++ src/main/java/exceptions/IllegalIndexException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class IllegalIndexException extends RuntimeException {
+    public IllegalIndexException(String message) {
+        super(message);
+    }
+}
--- src/main/java/org/skypro/Main.java
+++ src/main/java/org/skypro/Main.java
@@ -1,7 +1,17 @@
 package org.skypro;
 
+import stringList.StringList;
+import stringList.StringListImpl;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StringList stringList = new StringListImpl(2);
+        stringList.add("one");
+        stringList.add("two");
+        stringList.add("three");
+
+        stringList.remove("two");
+
+        System.out.println(stringList.toArray()[2]);
     }
 }
